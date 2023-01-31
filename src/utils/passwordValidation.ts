ea8a864278a9224145de4995e7ef9b406f984f82
tsentseus <tsentseus.askold@lll.kpi.ua>
2023-01-31 22:30:09 +0200

    regexp constants

--- src/utils/passwordValidation.ts
+++ src/utils/passwordValidation.ts
@@ -1,22 +1,26 @@
 import {IPassword} from "../interface";
-
+import {regexp} from '../constants/regexp'
 export const passwordValidation = (fields: IPassword,setState: Function) => {
     const { password } = fields
-    const lettersSymbolsDigits= /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W|_)/.test(password)
-    const lettersAndSymbols = /(?=.*[a-z])(?=.*\W|_)/.test(password)
-    const lettersAndDigits = /(?=.*\d)(?=.*[a-z])/.test(password)
-    const digitsAndSymbols = /(?=.*\d)(?=.*\W|_)/.test(password)
+    const { fullValidation, onlyLettersAndSymbols,onlyLettersAndDigits,onlyDigitsAndSymbols } = regexp
+
+    const lettersSymbolsDigits = fullValidation.test(password)
+    const lettersAndSymbols = onlyLettersAndSymbols.test(password)
+    const lettersAndDigits = onlyLettersAndDigits.test(password)
+    const digitsAndSymbols = onlyDigitsAndSymbols.test(password)
 
     if(password.length >= 8) {
         setState({...fields, easy: 'red', medium:'gray', hard: 'gray' })
 
-        if(lettersAndDigits || lettersAndSymbols || digitsAndSymbols) {
+    if(lettersAndDigits || lettersAndSymbols || digitsAndSymbols) {
             setState({...fields, easy: 'medium', medium: 'medium', hard: 'gray'})
         }
-        if(lettersSymbolsDigits) {
+
+    if(lettersSymbolsDigits) {
             setState({...fields, easy: 'green', medium: 'green', hard: 'green'})
         }
     }
+
     if(password.length < 8 && password.length > 0) {
         setState({...fields, easy: 'red', medium: 'red', hard: 'red'})
     }
